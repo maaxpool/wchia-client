144f00177a09027ccee5ba716cb1db34e73f103b
liyue <905121829@qq.com>
2021-09-01 14:34:27 +0800

    Home page banner descriction correct

--- dist/assets/js/chunk-2d208298.3adadd89.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d208298"],{a42a:function(n,t,e){"use strict";e.r(t);var u=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[n._v("404")])},a=[],c={data:function(){return{}}},r=c,l=e("2877"),i=Object(l["a"])(r,u,a,!1,null,null,null);t["default"]=i.exports}}]);
